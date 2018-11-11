80ba4dbf4814513aee1869a13199493414654213
pedro823 <grey.pedrinho@gmail.com>
2018-11-10 23:35:47 -0200

    Adds bomb.ino prototype

---
+++ bomb.ino
@@ -0,0 +1,96 @@
+/*
+  Blink
+
+  Turns an LED on for one second, then off for one second, repeatedly.
+
+  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
+  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
+  the correct LED pin independent of which board is used.
+  If you want to know what pin the on-board LED is connected to on your Arduino
+  model, check the Technical Specs of your board at:
+  https://www.arduino.cc/en/Main/Products
+
+  modified 8 May 2014
+  by Scott Fitzgerald
+  modified 2 Sep 2016
+  by Arturo Guadalupi
+  modified 8 Sep 2016
+  by Colby Newman
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/Blink
+*/
+
+#include <Wire.h>
+
+typedef struct {
+  int x;
+  int y;
+  int z;
+  int temperature;
+  int gx;
+  int gy;
+  int gz;
+} accelerometer_values;
+
+const static int MPU = 0x68;
+
+accelerometer_values get_accelerometer_values() {
+  accelerometer_values values;
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU, 14, true); // get data from sensor
+  values.x = Wire.read() << 8 | Wire.read();
+  values.y = Wire.read() << 8 | Wire.read();
+  values.z = Wire.read() << 8 | Wire.read();
+  values.temperature = Wire.read() << 8 | Wire.read();
+  values.gx = Wire.read() << 8 | Wire.read();
+  values.gy = Wire.read() << 8 | Wire.read();
+  values.gz = Wire.read() << 8 | Wire.read();
+  return values;
+}
+
+bool blue_light() {
+  // Accelerometer challenge
+  accelerometer_values accelerometer = get_accelerometer_values();
+}
+
+bool red_light() {
+  // Potentiometer challenge
+  return false;
+}
+
+bool green_light() {
+  // Unplugging a wire challenge
+  return false;
+}
+
+bool yellow_light() {
+  // Button challenge
+  return false;
+}
+
+void setup() {
+  pinMode(D7, OUTPUT);
+  // Initializes MPU
+  Wire.begin();
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B); Wire.write(0);
+  Wire.endTransmission(true);
+}
+
+void loop() {
+  if (blue_light()
+      && red_light()
+      && green_light()
+      && yellow_light()) {
+        defuse();
+  }
+  
+  digitalWrite(D7, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(D7, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+}
