455cb8bebc860e1d77cfffcceac0780c20b05798
Raphael R. Gusmao <raphael.rgusmao@gmail.com>
2018-11-11 07:40:33 -0200

    Potentiometer

--- bomb/bomb.ino
+++ bomb/bomb.ino
@@ -1,3 +1,11 @@
+/******************************************************************************
+ * Bomb                                                                       *
+ *                                                                            *
+ * Bruno R. Arico                                                             *
+ * Pedro L. Pereira                                                           *
+ * Raphael R. Gusmao                                                          *
+ ******************************************************************************/
+
 #include <Wire.h>
 #include <math.h>
 
@@ -12,6 +20,12 @@ typedef struct {
 } accelerometer_values;
 
 const static int MPU = 0x68;
+#define pot1 A1
+#define pot2 A2
+#define red_led A3
+
+long rnd_interval1;
+long rnd_interval2;
 
 accelerometer_values get_accelerometer_values() {
   accelerometer_values values;
@@ -30,7 +44,7 @@ accelerometer_values get_accelerometer_values() {
 }
 
 bool is_close_to(int value, int desired, int tolerance) {
-  return abs(value - desired) < tolerance;  
+  return abs(value - desired) < tolerance;
 }
 
 bool blue_light() {
@@ -41,20 +55,20 @@ bool blue_light() {
 
   // //Envia valor Y do acelerometro para a serial e o LCD
   // Serial.print(" | AcY = "); Serial.print(accel.y);
-   
+
   // //Envia valor Z do acelerometro para a serial e o LCD
   // Serial.print(" | AcZ = "); Serial.print(accel.z);
-   
+
   // //Envia valor da temperatura para a serial e o LCD
   // //Calcula a temperatura em graus Celsius
   // Serial.print(" | Tmp = "); Serial.print(accel.temperature/340.00+36.53);
-   
+
   // //Envia valor X do giroscopio para a serial e o LCD
   // Serial.print(" | GyX = "); Serial.print(accel.gx);
 
-  // //Envia valor Y do giroscopio para a serial e o LCD  
+  // //Envia valor Y do giroscopio para a serial e o LCD
   // Serial.print(" | GyY = "); Serial.print(accel.gy);
-   
+
   // //Envia valor Z do giroscopio para a serial e o LCD
   // Serial.print(" | GyZ = "); Serial.println(accel.gz);
 
@@ -65,6 +79,20 @@ bool blue_light() {
 
 bool red_light() {
   // Potentiometer challenge
+
+  float pot1_value = map(analogRead(pot1), 0, 915, 0, 900);
+  float pot2_value = map(analogRead(pot2), 0, 915, 0, 900);
+
+  Serial.print("Pot1: ");Serial.print(pot1_value);Serial.print("\n");
+  Serial.print("Pot2: ");Serial.print(pot2_value);Serial.print("\n\n");
+
+  if (rnd_interval1*100 < pot1_value && pot1_value <= (rnd_interval1+1)*100
+   && rnd_interval2*100 < pot2_value && pot2_value <= (rnd_interval2+1)*100) {
+    digitalWrite(red_led, HIGH);
+  } else {
+    digitalWrite(red_led, LOW);
+  }
+
   return true;
 }
 
@@ -80,6 +108,7 @@ bool yellow_light() {
 
 void setup() {
   Serial.begin(9600);
+  randomSeed(analogRead(0));
   // Initializes MPU
   Wire.begin();
   Wire.beginTransmission(MPU);
@@ -88,7 +117,14 @@ void setup() {
   if (error != 0) {
     Serial.write("WARNING: Accelerometer not found.");
   }
+
+  // Potentiometer
   pinMode(3, OUTPUT);
+  pinMode(pot1, INPUT);
+  pinMode(pot2, INPUT);
+  pinMode(red_led, OUTPUT);
+  rnd_interval1 = random(0, 9);
+  rnd_interval2 = random(0, 9);
 }
 
 void defuse() {
