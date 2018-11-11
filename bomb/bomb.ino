92ebcfc368cbd774eb398a2c151068fcea5e7c18
pedro823 <grey.pedrinho@gmail.com>
2018-11-11 02:37:13 -0200

    Adds accelerometer challenge

--- bomb/bomb.ino
+++ bomb/bomb.ino
@@ -23,6 +23,7 @@
 */
 
 #include <Wire.h>
+#include <math.h>
 
 typedef struct {
   int x;
@@ -52,40 +53,85 @@ accelerometer_values get_accelerometer_values() {
   return values;
 }
 
+bool is_close_to(int value, int desired, int tolerance) {
+  return abs(value - desired) < tolerance;  
+}
+
 bool blue_light() {
   // Accelerometer challenge
-  accelerometer_values accelerometer = get_accelerometer_values();
+  accelerometer_values accel = get_accelerometer_values();
+
+  // Serial.print("AcX = "); Serial.print(accel.x);
+
+  // //Envia valor Y do acelerometro para a serial e o LCD
+  // Serial.print(" | AcY = "); Serial.print(accel.y);
+   
+  // //Envia valor Z do acelerometro para a serial e o LCD
+  // Serial.print(" | AcZ = "); Serial.print(accel.z);
+   
+  // //Envia valor da temperatura para a serial e o LCD
+  // //Calcula a temperatura em graus Celsius
+  // Serial.print(" | Tmp = "); Serial.print(accel.temperature/340.00+36.53);
+   
+  // //Envia valor X do giroscopio para a serial e o LCD
+  // Serial.print(" | GyX = "); Serial.print(accel.gx);
+
+  // //Envia valor Y do giroscopio para a serial e o LCD  
+  // Serial.print(" | GyY = "); Serial.print(accel.gy);
+   
+  // //Envia valor Z do giroscopio para a serial e o LCD
+  // Serial.print(" | GyZ = "); Serial.println(accel.gz);
+
+  return is_close_to(accel.x, 11500, 4200)
+         && is_close_to(accel.y, -2000, 4200)
+         && is_close_to(accel.z, -12000, 4200);
 }
 
 bool red_light() {
   // Potentiometer challenge
-  return false;
+  return true;
 }
 
 bool green_light() {
   // Unplugging a wire challenge
-  return false;
+  return true;
 }
 
 bool yellow_light() {
   // Button challenge
-  return false;
+  return true;
 }
 
 void setup() {
-  pinMode(D7, OUTPUT);
+  Serial.begin(9600);
   // Initializes MPU
   Wire.begin();
   Wire.beginTransmission(MPU);
   Wire.write(0x6B); Wire.write(0);
-  Wire.endTransmission(true);
+  byte error = Wire.endTransmission(false);
+  if (error != 0) {
+    Serial.write("WARNING: Accelerometer not found.");
+  }
+  pinMode(3, OUTPUT);
+}
+
+void defuse() {
 }
 
 void loop() {
+  bool blue   = blue_light(),
+       red    = red_light(),
+       green  = green_light(),
+       yellow = yellow_light();
+
+  digitalWrite(3, blue ? HIGH : LOW);
+
   if (blue_light()
       && red_light()
       && green_light()
       && yellow_light()) {
         defuse();
   }
+
+  delay(500);
 }
